c5b72cc6dabb39e869f83d2cbc6bfa3a07ad9c38
Nicolas Eeckeloo <neeckeloo@gmail.com>
2015-03-07 15:21:04 +0100

    Fix unit tests

--- tests/Service/Auth0ServiceTest.php
+++ tests/Service/Auth0ServiceTest.php
@@ -7,16 +7,33 @@ class Auth0ServiceTest extends \PHPUnit_Framework_TestCase
 {
     public function testGetUserReturnAuth0UserEntity()
     {
+        $userInfo = ['user_id' => 123];
+
         $auth0SdkStub = $this->getMockBuilder('Auth0SDK\Auth0')
             ->disableOriginalConstructor()
             ->getMock();
         $auth0SdkStub
             ->method('getUserInfo')
-            ->will($this->returnValue([]));
+            ->will($this->returnValue($userInfo));
 
         $auth0Service = new Auth0Service($auth0SdkStub);
 
         $entity = $auth0Service->getUser();
         $this->assertInstanceOf('Riskio\Auth0Module\Entity\Auth0UserEntity', $entity);
+    }
+
+    public function testGetUserWhenUserInfoIsEmptyReturnNull()
+    {
+        $auth0SdkStub = $this->getMockBuilder('Auth0SDK\Auth0')
+            ->disableOriginalConstructor()
+            ->getMock();
+        $auth0SdkStub
+            ->method('getUserInfo')
+            ->will($this->returnValue(null));
+
+        $auth0Service = new Auth0Service($auth0SdkStub);
+
+        $entity = $auth0Service->getUser();
+        $this->assertNull($entity);
     }
 }
