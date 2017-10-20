bc212c0b837d23546b76cdfbb928d76a652eb34e
Nicolas Eeckeloo <neeckeloo@gmail.com>
2017-10-20 17:28:11 +0200

    Uses aliases in config

--- config/module.config.php
+++ config/module.config.php
@@ -1,23 +1,26 @@
 <?php
+
+use Riskio\Auth0Module\Factory;
+
 return [
     'authentication' => [
         'adapter' => 'Riskio\Auth0Module\AuthenticationAdapter',
     ],
 
     'auth0' => [
-        'account'       => null,
-        'token'         => null,
-        'client_id'     => null,
+        'account' => null,
+        'token' => null,
+        'client_id' => null,
         'client_secret' => null,
-        'redirect_uri'  => null,
+        'redirect_uri' => null,
     ],
 
     'service_manager' => [
         'factories' => [
-            'Riskio\Auth0Module\AuthenticationAdapter'  => 'Riskio\Auth0Module\Factory\AuthenticationAdapterFactory',
-            'Riskio\Auth0Module\Auth0Management'        => 'Riskio\Auth0Module\Factory\Auth0ManagementFactory',
-            'Riskio\Auth0Module\OAuth2\Client\Provider' => 'Riskio\Auth0Module\Factory\OAuthProviderFactory',
-            'Riskio\Auth0Module\Options\Auth0Options'   => 'Riskio\Auth0Module\Factory\Auth0OptionsFactory',
+            'Riskio\Auth0Module\AuthenticationAdapter' => Factory\AuthenticationAdapterFactory::class,
+            'Riskio\Auth0Module\Auth0Management' => Factory\Auth0ManagementFactory::class,
+            'Riskio\Auth0Module\OAuth2\Client\Provider' => Factory\OAuthProviderFactory::class,
+            'Riskio\Auth0Module\Options\Auth0Options' => Factory\Auth0OptionsFactory::class,
         ],
     ],
 ];
